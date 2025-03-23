01a04006f39b42f22135b12e2c06d1d8c72cbaa3
Stefan Schell <stefanschell@gmail.com>
2025-03-23 16:31:24 +1100

    working on simulator

--- src/simulator.rs
+++ src/simulator.rs
@@ -20,13 +20,27 @@ pub struct Options {
     pub prev_repayment_date: NaiveDateTime,
     pub repayment_cycle: Cycle,
     pub repayment_use_stash: bool,
-    pub schedule_end: NaiveDateTime,
+    pub schedule_end: Option<NaiveDateTime>,
     pub leftover_incoming: Option<NaiveDateTime>,
     pub leftover_amount: Option<f64>,
     pub leftover_repayment: Option<f64>,
     pub extra_win_amount: Option<f64>,
     pub extra_win_cycle: Option<Cycle>,
     pub extra_win_duration: Option<TimeDelta>,
+}
+
+pub struct ScheduleEntry {
+    pub date: NaiveDateTime,
+    pub loan_duration: TimeDelta,
+    pub schedule_duration: TimeDelta,
+    pub interest: f64,
+    pub redraw: f64,
+    pub repayment: f64,
+    pub stashed: f64,
+    pub principal: f64,
+    pub stash: f64,
+    pub extra_win_for_loan: f64,
+    pub extra_win_for_us: f64,
 }
 
 fn increment_date(date: &NaiveDateTime, cycle: &Cycle) -> NaiveDateTime {
@@ -69,4 +83,64 @@ fn increment_date(date: &NaiveDateTime, cycle: &Cycle) -> NaiveDateTime {
     }
 }
 
-pub fn simulate(options: &Options) -> () {}
+pub fn simulate(options: &Options) -> () {
+    let mut curr_date = options.schedule_start;
+
+    let stash = 0f64;
+
+    let extra_win_end = match options.extra_win_duration {
+        Some(extra_win_duration) => Some(options.schedule_start + extra_win_duration),
+        None => None,
+    };
+
+    let prev_extra_win_date = match &options.extra_win_cycle {
+        Some(_) => Some(curr_date),
+        None => None,
+    };
+
+    let mut schedule: Vec<ScheduleEntry> = vec![];
+    let mut owing_daily_history: Vec<f64> = vec![];
+
+    schedule.push(ScheduleEntry {
+        date: curr_date,
+        loan_duration: curr_date - options.loan_start,
+        schedule_duration: curr_date - options.schedule_start,
+        interest: 0.0,
+        redraw: 0.0,
+        repayment: 0.0,
+        stashed: 0.0,
+        principal: options.principal,
+        stash: 0.0,
+        extra_win_for_loan: 0.0,
+        extra_win_for_us: 0.0,
+    });
+
+    let mut principal = options.principal;
+
+    while (principal > 0f64
+        || options
+            .leftover_incoming
+            .is_some_and(|leftover_incoming| curr_date < leftover_incoming))
+        || extra_win_end.is_some_and(|extra_win_end| curr_date < extra_win_end)
+    {
+        curr_date = curr_date + Duration::days(1);
+
+        let maturity_is_today = match options.schedule_end {
+            Some(schedule_end) => curr_date >= schedule_end,
+            None => false,
+        };
+
+        let mut curr_interest: Option<f64> = None;
+        let mut curr_redraw: Option<f64> = None;
+        let mut curr_repayment: Option<f64> = None;
+        let mut curr_stashed: Option<f64> = None;
+        let mut curr_extra_win_for_loan: Option<f64> = None;
+        let mut curr_extra_win_for_us: Option<f64> = None;
+
+        // interest
+        // note: we keep track of the amount owning on a daily basis,
+        //       back to the previous interest calculation
+
+        owing_daily_history.push((principal - options.offset).max(0f64));
+    }
+}
